af6bd9cb182f528210fae7d569b377dbb5b52f84
wehttamocsalb <wehttamocsalb@gmail.com>
2022-02-28 01:35:29 +0800

    Fixed the statements and debug all possible user answers

--- index.js
+++ index.js
@@ -29,14 +29,15 @@ var log = console.log;
 					prompt(`And your reason(s) for a career shift?`), 
 					prompt(`What is/are your future goal(s)?`));
 		log(user);
-		document.getElementById("head").innerHTML = user;
+		var h1 = document.getElementById("head");
+		h1.innerHTML = user;
 
 		/*Button appears after the prompts <3*/
-		let btn = document.createElement("button");
+		var btn = document.createElement("button");
 		btn.innerHTML = "Submit";
 		btn.type = "submit";
 		btn.name = "formBtn";
-		document.body.appendChild(btn);
+		document.body.appendChild(btn)[4];
 
 		/*Functionalities of the button*/
 		/*var attr = document.createAttribute("onclick");
@@ -52,16 +53,16 @@ var log = console.log;
 				
 			alert(`We will now give you choice, and the following choice will benefit you. Are you ready?`);
 				// 1Q
-				let userAnswer = prompt(`Type "y" to proceed. Otherwise, type "n".`);
+				var userAnswer = prompt(`Type "y" to proceed. Otherwise, type "n".`);
 				// First Answer Yes to First Question at first; 1Q1A
 				if(userAnswer === `y` || userAnswer === `Y`)
 				{
 						// 1Q1A2Q
-						let userAnswer1 = prompt(`The first benefit is for our new customers. Are you our existing customer? Type "y" to agree. Otherwise, type "n".`)
+						var userAnswer1 = prompt(`The first benefit is for our new customers. Are you our existing customer? Type "y" to agree. Otherwise, type "n".`)
 						{
 								// 1Q1A2Q1A
 								if(userAnswer1 === `y` || userAnswer1 === `Y`){
-									let userAnswer2 = prompt(`For existing customers; As our first version prototype of prepaid products, we are offering a one-time bonus discount of -50% to switch to our monthly subscription; Would you like to hear more? Type "y" to agree. Otherwise, type "n".`)
+									var userAnswer2 = prompt(`For existing customers; As our first version prototype of prepaid products, we are offering a one-time bonus discount of -50% to switch to our monthly subscription; Would you like to hear more? Type "y" to agree. Otherwise, type "n".`)
 										if(userAnswer2 === `y` || userAnswer2 === `Y`){
 										prompt(`Please input your email or phone number so our agents can further assist you.`)
 										alert(`You will be assisted by our customer service concierge shortly! We wholeheartedly thank you for your support! And we are more than looking forward to serving you more!`)
@@ -76,22 +77,80 @@ var log = console.log;
 								};
 						
 						};
-						if(userAnswer === `n` || userAnswer === `N`){  
-									alert(`I'm sorry you don't want to hear more of our benefits. Thank you for your interest in visiting our website!`)}
+				}
+				else if(userAnswer === `n` || userAnswer === `N`){  
+									alert(`I'm sorry you don't want to hear more of our benefits. Thank you for your interest in visiting our website!`)
+									userAnswer1 = "n";
+				}
+
+				else{
+					alert(`I'm sorry I could not read your answer, please press submit again and communicate in either "y" or "n" response. Thank you for your understanding.`);
+					return userAnswer1 = `n`; 
 				};
+			
+				// Ternary Operator
 
-		// Ternary Operator
+				// Convert the conditional statement with the use of Ternary Operator
+					let minute = prompt(`Oh! And before we actually forget, do you have a minute? Y/N?`);
 
-		// Convert the conditional statement with the use of Ternary Operator
-			let minute = prompt(`Oh! And before we actually forget, do you have a minute? Y/N?`);
+					prompt((minute === `y` || minute === `Y`) ? `Can you leave us a  short comment about your experience with us so far?` : `This will be very short, I promise. Just a quick feedback/comment will do about your experience with us! Thank you!`);
 
-			prompt((minute === `y` || minute === `Y`) ? `Can you leave us a  short comment about your experience with us so far?` : `This will be very short, I promise. Just a quick feedback/comment will do about your experience with us! Thank you!`);
+					alert(`From the bottom of our hearts, thank you so very much for your time!`);
 
-			alert(`From the bottom of our hearts, thank you so very much for your time!`);
+
+		h1.remove();
+		var btn1 = document.getElementsByTagName("button")[0];
+		btn1.remove();
+		var h33 = document.getElementById("head3");
+		var p11 = document.getElementById("para1");
+		var p22 = document.getElementById("para2");
+		var p33 = document.getElementById("para3");
+				
+		// Switch case statement
+		// Create a conditional statements that will give the user options base on their needs followed by executing the codes based on the chosen option
+
+					// Addtl buttons for options
+					var btnOp1 = document.createElement("button");
+					btnOp1.innerHTML = "Choose Package 1";
+					btnOp1.type = "submit";
+					btnOp1.name = "formBtn";
+					document.body.appendChild(btnOp1)[4];
+					
+
+					var btnOp2 = document.createElement("button");
+					btnOp2.innerHTML = "Choose Package 2";
+					btnOp2.type = "submit";
+					btnOp2.name = "formBtn";
+					document.body.appendChild(btnOp2)[6]
+
+
+			switch(userAnswer1){
+				case `y`: 
+					h33.innerHTML = "Thank you for being a loyal customer!";
+					p11.innerHTML = "As a sincere gift! Please choose a package from us!";
+					p22.innerHTML = "Package 1: 2k Diamond Ring + 1 Your Personal Choice of Lamborghini";
+					p33.innerHTML = "Package 2: Ten Million USD Cash + 1 Year Vacation All Expenses Paid" ;
+					break; 
+				case `n`:
+					alert(`Goodbye Now!`);
+					btnOp1.remove();
+					btnOp2.remove();
+					break;
+				default: 
+					alert(`It's only "y" or "n", are you an alien?`);
+					btnOp1.remove();
+					btnOp2.remove();
+			};
+/*			console.log(userAnswer);
+			console.log(userAnswer1);
+			console.log(userAnswer2);*/
 
 		});
 		
 
+
+
+	
 
 
 
